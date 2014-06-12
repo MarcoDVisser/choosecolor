570e47e47124beaccd66aa442f86faf73c034022
Marco Visser <marco.d.visser@gmail.com>
2014-06-12 10:06:31 +0200

    Made the move towards roxygen in this version

--- R/choosecolor.R
+++ R/choosecolor.R
@@ -19,15 +19,29 @@
 #  
 #  
 
-# This function plots a decent selection of colors in a grid
-# then allows the user to pick n colors, and returns the respective
-# Hex code. This version gives a much prettier grid of colors 
-# using Hue and lightness values than the older color.pick function.
-# Usage: color.choose 
-# detail = smoothness of color chart
-# pick = logical, should the function return a hex code of the color picked
-# or just return the locations of the colors (for use in other functions).
-
+##' color.choose
+##'
+##' color.choose helps to quickly select colors from a grid for usage in R
+##' graphics. See the example below for usage details. This version gives a
+##' much prettier grid of colors in a range of hue and lightness values compared
+##' to the simpler color.pick function.
+##'
+##' @title Choose colors interactively from a grid
+##' @param detail an integer specifying the smoothness of color chart.
+##' In essence it gives the NxN dimensions of the displayed color grid,
+##' which in turn dictates how many colors are displayed. Large values may slow
+##' down the process. Defaults to 200 (40 000 colors).
+##' 
+##' @param pick logical, whether or not the user should be allowed to select a 
+##' color. If pick == FALSE locations of the colors are returned.  This may be
+##' useful if you want to pick colors from within another
+##' function.
+##' @author Marco D. Visser
+##' @examples
+##' # pick a color
+##' mycol<-color.choose()
+##' plot(rnorm(100),col=mycol)
+##' @export
 color.choose<-function(detail=100,pick=TRUE){
 	# dimensions
 	N<-detail
@@ -54,31 +68,48 @@ color.choose<-function(detail=100,pick=TRUE){
 	else {return(cbind(ColVals,locations))}
 }
 
-# The following function lets you choose a range of colors 
-# to convert to a palette function
-# usage: MyPallete<-palette.picker()
-# n = number of colors to pick for your pallete
-# detail = smoothness of color plot (large values may slow the process
-# 		   down considerably.
-# 
 
-# Note this is the quick and dirty version which can be 
-# made much more efficient
 
-palette.picker=function(n=6,detail=100){
+##' palette.picker
+##'
+##' palette.picker aids in the selection of colors from a grid
+##' and builds a palette with colorRampPalette for usage in R
+##' graphics.  See the example below for usage details. 
+##'
+##' @title Choose a palette interactively from a grid
+##' @param n an integer specifying how many colors to make a pallete from.
+##' @param detail an integer specifying the smoothness of color chart.
+##' In essence it gives the NxN dimensions of the displayed color grid,
+##' which in turn dictates how many colors are displayed. Large values may slow
+##' down the process. Defaults to 100 (10 000 colors).
+##' 
+##' @author Marco D. Visser
+##' @examples
+##' load volcano data
+##' data(volcano)
+##' #Select 5 colors to make a palette
+##' Mypalette<-palette.picker(n=5)
+##' create an image with the new palette
+##" image(x,y,volcano,col=Mypalette(1000))
+##' image(volcano,col=MyPallete(1000))
+##' @export
+palette.picker <- function(n=6,detail=100){
 
-ColPick<-character(n)
-
-	for (i in 1:n){
-	ColPick[i]<-color.choose(detail=detail,pick=TRUE)
-	}
-
-	colorRampPalette(ColPick, space = "Lab")
+  ColPick<-character(n)
+  
+  for (i in 1:n){
+    ColPick[i]<-color.choose(detail=detail,pick=TRUE)
+  }
+  
+  colorRampPalette(ColPick, space = "Lab")
 }
 
-# Helper function for color.wheel, plots a color wheel
-
-
+## Helper function for color.wheel, plots a color wheel
+## @param Colcode a vector of color codes
+## @param radius specific the size of the wheel (and arrows)
+## @param detail color detail on wheel
+## @arrowloc color location of arrow
+## @arrowcol arrow color
 plotWheel<-function(Colcode,radius=0.5,detail,arrowloc,
 arrowcol){
 	par(mar=c(0,0,0,0))
@@ -90,68 +121,85 @@ arrowcol){
 }
 
 
-# The following function, plot a color wheel,
-# and gives even more control on the
-# level of color detail that can be chosen.
-# First it lets the user choose a color
-# Then it allow further detail on lightness levels 
-#
 
-# usage: color.wheel()
-# detail = smoothness of color plot (large values may slow the process
-# 		   down considerably.
-# 
-
+##' color.wheel
+##'
+##'  color.wheel allows greater detail in color selection compared to
+##'  color.choose(). The user first chooses the color and then the 
+##'  lightness of the color.
+##'
+##' @title Choose a color interactively from a color wheel
+##' @param detail an integer specifying the smoothness of color chart.
+##' In essence this specifies the number of colors plotted on the wheel
+##' (large values may slow the process down considerably).
+##' @author Marco D. Visser
+##' @examples
+##' # Get more detail in choice with the color wheel
+##' mycol<-color.wheel()
+##' plot(rnorm(100),col=mycol)
+##' @export
 color.wheel<-function(detail=200){
 
-	#Values
-	x<-seq(-pi,pi,length.out=detail)
-	values<-seq(0,1,length.out=detail)
-	HueVals<-sapply(1:detail,function(X) hsv(h=values,s=1,v=1))
-
-	#plot color wheel
-	plotWheel(Colcode=HueVals,radius=0.65,detail=detail,
-	arrowloc=c(0,0.5),arrowcol='white')
-
-	print("Choose a color")
-	pick<-locator(1)
-	HueValPick<-values[findInterval(atan2(pick$x,pick$y),x)]
-	# Reset color values
-	ColVals<-sapply(1:detail,function(X) 
-	hsv(h=HueValPick,s=1,v=values))
-
-	#Replot color wheel
-	par(new=T)
-	newarrowloc<-c(
-	sin(atan2(pick$x,pick$y))*sqrt(pick$x^2+pick$y^2)*0.85,
-	cos(atan2(pick$x,pick$y))*sqrt(pick$x^2+pick$y^2)*0.85)
-
-	plotWheel(Colcode=ColVals,radius=0.45,detail=detail,
-	arrowloc=newarrowloc,arrowcol='grey')
-
-	print("Choose a lightness")
-	pick<-locator(1)
-	LightValPick<-findInterval(atan2(pick$x,pick$y),x)
-	return(ColVals[LightValPick])
+                                        #Values
+  x<-seq(-pi,pi,length.out=detail)
+  values<-seq(0,1,length.out=detail)
+  HueVals<-sapply(1:detail,function(X) hsv(h=values,s=1,v=1))
+  
+                                        #plot color wheel
+  plotWheel(Colcode=HueVals,radius=0.65,detail=detail,
+            arrowloc=c(0,0.5),arrowcol='white')
+  
+  print("Choose a color")
+  pick<-locator(1)
+  HueValPick<-values[findInterval(atan2(pick$x,pick$y),x)]
+                                        # Reset color values
+  ColVals<-sapply(1:detail,function(X) 
+                  hsv(h=HueValPick,s=1,v=values))
+  
+                                        #Replot color wheel
+  par(new=T)
+  newarrowloc<-c(
+                 sin(atan2(pick$x,pick$y))*sqrt(pick$x^2+pick$y^2)*0.85,
+                 cos(atan2(y= , x= )(pick$x,pick$y))*sqrt(pick$x^2+pick$y^2)*0.85)
+  
+  plotWheel(Colcode=ColVals,radius=0.45,detail=detail,
+            arrowloc=newarrowloc,arrowcol='grey')
+  
+  print("Choose a lightness")
+  pick<-locator(1)
+  LightValPick<-findInterval(atan2(pick$x,pick$y),x)
+  return(ColVals[LightValPick])
+  
 }
 
-# R color name picker, very simple color picker
-# returns the R color names from the list of colors in 
-# colors()
 
+##' color.pick
+##'
+##' color.pick allows a user to quickly select a color from
+##' a list of R color names. This function is depreciated and
+##' may disappear in subsequent version of the package. 
+##'
+##' @title Pick colors interactively from a named list
+##' 
+##' @author Marco D. Visser
+##' @examples
+##' # pick a color
+##' mycol<-color.pick()
+##' plot(rnorm(100),col=mycol)
+##' @export
 color.pick<-function(){
-# colors
-ColVec<-colors()
-# dimensions
-N<-ceiling(sqrt(length(ColVec)))
-filler<- rep("white",(N^2)-length(ColVec))
-# colorgrid
-locations<-expand.grid(1:N,1:N)
-ColGrid<-matrix(c(ColVec,filler),ncol=N,nrow=N)
-par(mar=c(0,0,0,0))
-image(1:N,1:N,t(matrix(1:N^2,ncol=N,nrow=N)),col=ColGrid)
-pick<-locator(1)
- dist<-apply(locations,1,
- function(X) sqrt((pick$x-X[2])^2+(pick$y-X[1])^2))
- return(ColVec[which(dist==min(dist))])
+                                        # colors
+  ColVec<-colors()
+                                        # dimensions
+  N<-ceiling(sqrt(length(ColVec)))
+  filler<- rep("white",(N^2)-length(ColVec))
+                                        # colorgrid
+  locations<-expand.grid(1:N,1:N)
+  ColGrid<-matrix(c(ColVec,filler),ncol=N,nrow=N)
+  par(mar=c(0,0,0,0))
+  image(1:N,1:N,t(matrix(1:N^2,ncol=N,nrow=N)),col=ColGrid)
+  pick<-locator(1)
+  dist<-apply(locations,1,
+              function(X) sqrt((pick$x-X[2])^2+(pick$y-X[1])^2))
+  return(ColVec[which(dist==min(dist))])
 }
